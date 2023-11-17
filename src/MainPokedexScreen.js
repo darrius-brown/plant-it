696830b8c0747dcadbf71fd06bbadf25a8240bc7
darriub15 <darriusabrown@gmail.com>
2023-11-17 13:49:11 -0800

    blank pokedex working

--- src/MainPokedexScreen.js
+++ src/MainPokedexScreen.js
@@ -1,11 +1,9 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { getPokemonCount } from './API/PokeAPIs'
-import Button from 'react-bootstrap/Button'
-
 
 const MainPokedexScreen = () => {
-  const [numberOfPokemon, setnumberOfPokemon] = useState()
-
+  const [numberOfPokemon, setnumberOfPokemon] = useState();
+  const [pokedexList, setPokedexList] = useState();
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -19,12 +17,31 @@ const MainPokedexScreen = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setPokedexList(Array(numberOfPokemon).fill('______________________'));
+  }, [numberOfPokemon]);
+
+  const renderPokedexList = () => {
+    if (!pokedexList) {
+      return (
+        <h1>Loading....</h1>
+      )
+    }
+    return pokedexList.map((item, index) => {
+      return (
+      <li key={index}>{item}</li>
+      )
+    })
+  };
+
   return (
     <div>
-    <h1>MainPokedexScreen</h1>
-    <Button onClick={() => {console.log(numberOfPokemon)}}></Button>
+      <h1>MainPokedexScreen</h1>
+      <ol>
+        {renderPokedexList()}
+      </ol>
     </div>
   )
-}
+};
 
 export default MainPokedexScreen
